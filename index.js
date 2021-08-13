68a7018fc1c371f404618bbda0fa2469bd585230
cc <votaryofturms@icloud.com>
2021-08-13 15:45:56 -0400

    task 4 and 5

--- index.js
+++ index.js
@@ -243,10 +243,17 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function get20s(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]['years'].split('-')[0] > 1900 && array[i]['years'].split('-')[1] < 2000) {
+      newArray.push(array[i]['name'])
+    }
 
+  }
+  return newArray;
+}
+console.log('task 4', get20s(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -258,10 +265,11 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array.length;
 }
-
+console.log('task 5', removeArtist(artists, 0))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
